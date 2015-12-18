84fee6077d5ad83f10da94593b1938213551f229
w1zgam3r <jflaquer@udel.edu>
2015-12-18 18:53:34 -0500

    Commit

--- FinalExamQuestions/src/QuestionE/Student_Test.java
+++ FinalExamQuestions/src/QuestionE/Student_Test.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.UUID;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -40,9 +42,19 @@ public class Student_Test {
 			e.printStackTrace();
 		}
 		Student stu1 = new Student("Bert", "Randall", "Gibbons", dBirthDate, "214 Labrador Lane", "302-893-6889", "bgibbons@udel.edu","MIS");
+		Student stu2 = new Student("Jael", "Alejandro", "Flaquer", dBirthDate, "123 Congressional Dr", "555-555-5555", "jflaquer@udel.edu", "CIS");
+		Student stu3 = new Student("Mario", "Alejandro", "Flaquer", dBirthDate, "123 Congressional Dr", "555-555-5555", "jflaquer@udel.edu", "CIS");
+		Student stu4 = new Student("Belkis", "Alejandro", "Flaquer", dBirthDate, "123 Congressional Dr", "555-555-5555", "jflaquer@udel.edu", "CIS");
+		Student stu5 = new Student("Alberto", "Alejandro", "Flaquer", dBirthDate, "123 Congressional Dr", "555-555-5555", "jflaquer@udel.edu", "CIS");
+		
+		HashMap<UUID, Student> hm = new HashMap<UUID, Student>();
+		hm.put(stu1.getPersonID(), stu1);
+		hm.put(stu2.getPersonID(), stu2);
+		hm.put(stu3.getPersonID(), stu3);
+		hm.put(stu4.getPersonID(), stu4);
+		hm.put(stu5.getPersonID(), stu5);
 
-
-		assertTrue(1==1);
+		assertTrue(hm.size()==5);
 	}
 
 }
